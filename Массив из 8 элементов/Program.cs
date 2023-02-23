3d862cd8388c44e90e96a8bb7bd2ea2fde4275d8
Aleksandr <aleksandr-s86>
2023-02-23 11:41:50 +0300

    массив

---
+++ Массив из 8 элементов/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//6, 1, 33 -> [6, 1, 33]
+
+int[]array = Massive(8);
+Console.WriteLine($"[{String.Join(", ",array)}]");
+
+for(int i=0;i<array.Length-1;i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+Console.Write($"{array[array.Length-1]}");
+
+int[]Massive(int size)
+{
+    int[]result=new int[size];
+    for(int i=0;i<size;i++)
+    {
+        result[i] = new Random().Next(99);
+    }
+    return result;
+    }
