cdde6c51dc33585e30187d43178f52700c7528fe
Aleksandr <aleksandr-s86>
2023-02-23 10:51:58 +0300

    сумма цифр в числе

---
+++ Сумма цифр в числе/Program.cs
@@ -0,0 +1,22 @@
+﻿ //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+Console.WriteLine("Введите любое число: ");
+int num = int.Parse(Console.ReadLine()!);
+int Sumnum(int num)
+{
+    int res =0;
+    while(num>0)
+    {
+        res+=num%10;
+        num =num/10;
+    }
+    return res;
+}
+Console.WriteLine($"Сумма цифр в числе равна: {Sumnum(num)}");
+
